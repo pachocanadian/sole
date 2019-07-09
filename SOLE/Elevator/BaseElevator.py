c1448ab681a7e72dd2aceb36dd3a1e7dfa5c0194
Mike Carr <mcarr@pachogrande.com>
2019-07-08 19:54:16 -0700

    fleshed out get()/set() structure for elevator

--- SOLE/Elevator/BaseElevator.py
+++ SOLE/Elevator/BaseElevator.py
@@ -2,8 +2,29 @@ import uuid
 
 
 class BaseElevator:
-    def __init__(self):
+    def __init__(self, attributes=None):
+        """init() with no parameters or init(dict) can specify a dictionary of attributes"""
         self._uuid = uuid.uuid1()
+        self.attribute = dict()
+        if attributes is not None:
+            for key in attributes:
+                self.set(key, attributes[key])
+
+    def set(self, name, value):
+        """set() will set the given attribute for the object. Will perform basic sanity checks on the attribute itself."""
+        if name == "uuid":
+            raise Exception("cannot manually set uuid for an object of this type")
+
+        self.attribute[name] = value
+        return self
+
+    def get(self, name):
+        """get(attr) will return attribute attr for the object or empty string if not"""
+        if name in self.attribute:
+            return self.attribute[name]
+        else:
+            return ""
 
     def uuid(self):
+        """uuid() will return a unique identifier for the object"""
         return self._uuid
