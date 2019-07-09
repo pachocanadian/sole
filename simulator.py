cc0e881d6cbde58130de6fa3723afe1d9d71c48d
Mike Carr <mcarr@pachogrande.com>
2019-07-08 20:13:08 -0700

    fleshing out the pseudocode a bit more

--- simulator.py
+++ simulator.py
@@ -2,17 +2,20 @@ from pprint import pprint
 import SOLE
 
 
-b = SOLE.Elevator.SimpleElevator()
-print(b)
-
 floorLabels = ["Lobby", 1, 2, 3]
 floors = []
 for floorLabel in floorLabels:
-    f = SOLE.Floor.SimpleFloor({"label": floorLabel})
+    f = SOLE.Floor.SimpleFloor({"label": floorLabel, "height": 2.75})
     floors.append(f)
 
 for x in floors:
     print(x)
+
+basic_elevator = SOLE.Elevator.SimpleElevator(
+    {"floors": floors, "location": "Lobby", "status": "waiting"}
+)
+print(basic_elevator)
+
 
 d = SOLE.Person.SimplePerson({"name": "Foo"})
 print(d)
@@ -20,5 +23,5 @@ print(d)
 e = SOLE.Person.SimplePerson()
 print(e)
 
-a = SOLE.Building.SimpleBuilding({"floors": floors})
+a = SOLE.Building.SimpleBuilding({"floors": floors, "elevators": [basic_elevator]})
 print(a)
