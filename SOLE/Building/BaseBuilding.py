0e121daa0fa88f3648ac7b471f6f94b29c1f582e
Mike Carr <mcarr@pachogrande.com>
2019-07-08 19:58:25 -0700

    changed to get()/set() logic

--- SOLE/Building/BaseBuilding.py
+++ SOLE/Building/BaseBuilding.py
@@ -2,20 +2,29 @@ import uuid
 
 
 class BaseBuilding:
-    def __init__(self):
+    def __init__(self, attributes=None):
+        """init() with no parameters or init(dict) can specify a dictionary of attributes"""
         self._uuid = uuid.uuid1()
+        self.attribute = dict()
+        if attributes is not None:
+            for key in attributes:
+                self.set(key, attributes[key])
 
-    def getFloors(self):
-        return self.floors
+    def set(self, name, value):
+        """set() will set the given attribute for the object. Will perform basic sanity checks on the attribute itself."""
+        if name == "uuid":
+            raise Exception("cannot manually set uuid for an object of this type")
 
-    def setFloors(self, f):
-        self.floors = f
+        self.attribute[name] = value
+        return self
 
-    def getElevators(self):
-        return self.elevators
-
-    def setElevators(self, e):
-        self.elevators = e
+    def get(self, name):
+        """get(attr) will return attribute attr for the object or empty string if not"""
+        if name in self.attribute:
+            return self.attribute[name]
+        else:
+            return ""
 
     def uuid(self):
+        """uuid() will return a unique identifier for the object"""
         return self._uuid
