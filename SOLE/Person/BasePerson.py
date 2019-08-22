280ab537718a6e74aabd93113f0e280b3ea1da00
quark <christian.pearson@stemist.ca>
2019-08-21 18:05:46 -0700

    Fixing unload() and load() and adding a test in the tests file. Code currently broken.

--- SOLE/Person/BasePerson.py
+++ SOLE/Person/BasePerson.py
@@ -37,16 +37,20 @@ class BasePerson:
 
         # Ask all elevators if they are carrying this person; if so then remove person.
         for elevator in SOLE.BaseBuilding.default_attributes["elevators"]:
-            if self.id in elevator.get("carrying"):
-                elevator.set(carrying.remove(self.id))
+            if self.get("id") in elevator.get("carrying"):
 
-    def load(self):
+
+    def load(self, elevator):
         """load() will add person to an elevator."""
 
-        # Ask all elevators if they are carrying this person; if not then add person.
-        for elevator in SOLE.BaseBuilding.default_attributes["elevators"]:
-            if self.id in elevator.get("carrying"):
-                elevator.set(carrying.append(self.id))
+        # Ask elevator if it is carrying this person; if not then add person.
+        if self.get("id") not in elevator.get("carrying"):
+            elevator.set(
+                self.get("carrying"), self.get("carrying").append(self.get("id"))
+            )
+            return
+
+        raise Exception("Invalid elevator.")
 
     def tick(self):
         """tick() will advance one step for this object and any/all objects contained by it"""
--- test_sole.py
+++ test_sole.py
@@ -5,142 +5,176 @@ import SOLE
 
 # BEGIN BASEBUILDING TESTS
 
+
 def test_BASEBUILDING_getset_string():
     a = SOLE.Building.BaseBuilding()
-    v = 'var'
-    a.set('test_string', v)
-    assert (a.get('test_string') == v), "set/get don't match for string type"
+    v = "var"
+    a.set("test_string", v)
+    assert a.get("test_string") == v, "set/get don't match for string type"
+
 
 def test_BASEBUILDING_getset_float():
     a = SOLE.Building.BaseBuilding()
     v = 3.14159
-    a.set('test_float', v) 
-    assert (a.get('test_float') == v), "set/get don't match for float type"
+    a.set("test_float", v)
+    assert a.get("test_float") == v, "set/get don't match for float type"
+
 
 def test_BASEBUILDING_getset_list():
     a = SOLE.Building.BaseBuilding()
     v = [1, 2, 3]
-    a.set('test_list', v)
-    assert (a.get('test_list') == v), "set/get don't match for list type"
+    a.set("test_list", v)
+    assert a.get("test_list") == v, "set/get don't match for list type"
+
 
 def test_BASEBUILDING_getset_dict():
     a = SOLE.Building.BaseBuilding()
-    v = { 'a': 'b', 'c': 'd' }
-    a.set('test_dict', v)
-    assert (a.get('test_dict') == v), "set/get don't match for dict type"
+    v = {"a": "b", "c": "d"}
+    a.set("test_dict", v)
+    assert a.get("test_dict") == v, "set/get don't match for dict type"
+
 
 def test_BASEBUILDING_getset_iscopy():
     a = SOLE.Building.BaseBuilding()
-    ov = 'foo'
-    a.set('test_iscopy', ov)
-    nv = a.get('test_iscopy')
-    assert (nv is ov), "set/get are returing a copy rather than the original object"
+    ov = "foo"
+    a.set("test_iscopy", ov)
+    nv = a.get("test_iscopy")
+    assert nv is ov, "set/get are returing a copy rather than the original object"
+
 
 # END BASEBUILDING TESTS
 
 # BEGIN BASEELEVATOR TESTS
 
+
 def test_BASEELEVATOR_getset_string():
     a = SOLE.Elevator.BaseElevator()
-    v = 'var'
-    a.set('test_string', v)
-    assert (a.get('test_string') == v), "set/get don't match for string type"
+    v = "var"
+    a.set("test_string", v)
+    assert a.get("test_string") == v, "set/get don't match for string type"
+
 
 def test_BASEELEVATOR_getset_float():
     a = SOLE.Elevator.BaseElevator()
     v = 3.14159
-    a.set('test_float', v) 
-    assert (a.get('test_float') == v), "set/get don't match for float type"
+    a.set("test_float", v)
+    assert a.get("test_float") == v, "set/get don't match for float type"
+
 
 def test_BASEELEVATOR_getset_list():
     a = SOLE.Elevator.BaseElevator()
     v = [1, 2, 3]
-    a.set('test_list', v)
-    assert (a.get('test_list') == v), "set/get don't match for list type"
+    a.set("test_list", v)
+    assert a.get("test_list") == v, "set/get don't match for list type"
+
 
 def test_BASEELEVATOR_getset_dict():
     a = SOLE.Elevator.BaseElevator()
-    v = { 'a': 'b', 'c': 'd' }
-    a.set('test_dict', v)
-    assert (a.get('test_dict') == v), "set/get don't match for dict type"
+    v = {"a": "b", "c": "d"}
+    a.set("test_dict", v)
+    assert a.get("test_dict") == v, "set/get don't match for dict type"
+
 
 def test_BASEELEVATOR_getset_iscopy():
     a = SOLE.Elevator.BaseElevator()
-    ov = 'foo'
-    a.set('test_iscopy', ov)
-    nv = a.get('test_iscopy')
-    assert (nv is ov), "set/get are returing a copy rather than the original object"
+    ov = "foo"
+    a.set("test_iscopy", ov)
+    nv = a.get("test_iscopy")
+    assert nv is ov, "set/get are returing a copy rather than the original object"
+
 
 # END BASEELEVATOR TESTS
 
 # BEGIN BASEFLOOR TESTS
 
+
 def test_BASEFLOOR_getset_string():
     a = SOLE.Floor.BaseFloor()
-    v = 'var'
-    a.set('test_string', v)
-    assert (a.get('test_string') == v), "set/get don't match for string type"
+    v = "var"
+    a.set("test_string", v)
+    assert a.get("test_string") == v, "set/get don't match for string type"
+
 
 def test_BASEFLOOR_getset_float():
     a = SOLE.Floor.BaseFloor()
     v = 3.14159
-    a.set('test_float', v) 
-    assert (a.get('test_float') == v), "set/get don't match for float type"
+    a.set("test_float", v)
+    assert a.get("test_float") == v, "set/get don't match for float type"
+
 
 def test_BASEFLOOR_getset_list():
     a = SOLE.Floor.BaseFloor()
     v = [1, 2, 3]
-    a.set('test_list', v)
-    assert (a.get('test_list') == v), "set/get don't match for list type"
+    a.set("test_list", v)
+    assert a.get("test_list") == v, "set/get don't match for list type"
+
 
 def test_BASEFLOOR_getset_dict():
     a = SOLE.Floor.BaseFloor()
-    v = { 'a': 'b', 'c': 'd' }
-    a.set('test_dict', v)
-    assert (a.get('test_dict') == v), "set/get don't match for dict type"
+    v = {"a": "b", "c": "d"}
+    a.set("test_dict", v)
+    assert a.get("test_dict") == v, "set/get don't match for dict type"
+
 
 def test_BASEFLOOR_getset_iscopy():
     a = SOLE.Floor.BaseFloor()
-    ov = 'foo'
-    a.set('test_iscopy', ov)
-    nv = a.get('test_iscopy')
-    assert (nv is ov), "set/get are returing a copy rather than the original object"
+    ov = "foo"
+    a.set("test_iscopy", ov)
+    nv = a.get("test_iscopy")
+    assert nv is ov, "set/get are returing a copy rather than the original object"
+
 
 # END BASEFLOOR TESTS
 
 # BEGIN BASEPERSON TESTS
 
+
 def test_BASEPERSON_getset_string():
     a = SOLE.Person.BasePerson()
-    v = 'var'
-    a.set('test_string', v)
-    assert (a.get('test_string') == v), "set/get don't match for string type"
+    v = "var"
+    a.set("test_string", v)
+    assert a.get("test_string") == v, "set/get don't match for string type"
+
 
 def test_BASEPERSON_getset_float():
     a = SOLE.Person.BasePerson()
     v = 3.14159
-    a.set('test_float', v) 
-    assert (a.get('test_float') == v), "set/get don't match for float type"
+    a.set("test_float", v)
+    assert a.get("test_float") == v, "set/get don't match for float type"
+
 
 def test_BASEPERSON_getset_list():
     a = SOLE.Person.BasePerson()
     v = [1, 2, 3]
-    a.set('test_list', v)
-    assert (a.get('test_list') == v), "set/get don't match for list type"
+    a.set("test_list", v)
+    assert a.get("test_list") == v, "set/get don't match for list type"
+
 
 def test_BASEPERSON_getset_dict():
     a = SOLE.Person.BasePerson()
-    v = { 'a': 'b', 'c': 'd' }
-    a.set('test_dict', v)
-    assert (a.get('test_dict') == v), "set/get don't match for dict type"
+    v = {"a": "b", "c": "d"}
+    a.set("test_dict", v)
+    assert a.get("test_dict") == v, "set/get don't match for dict type"
+
 
 def test_BASEPERSON_getset_iscopy():
     a = SOLE.Person.BasePerson()
-    ov = 'foo'
-    a.set('test_iscopy', ov)
-    nv = a.get('test_iscopy')
-    assert (nv is ov), "set/get are returing a copy rather than the original object"
+    ov = "foo"
+    a.set("test_iscopy", ov)
+    nv = a.get("test_iscopy")
+    assert nv is ov, "set/get are returning a copy rather than the original object"
+
+
+def test_BASEPERSON_load_string():
+    a = SOLE.Person.BasePerson()
+    v = "1234"
+    a.set("id", v)
+
+    e = SOLE.Elevator.BaseElevator()
+    a.load(e)
+    assert (
+        v in e.default_attributes["carrying"]
+    ), "Person.load(elevator) did not load elevator"
+
 
 # END BASEPERSON TESTS
-
-
