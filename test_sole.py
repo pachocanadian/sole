15dd2be49ec9d42ac9ce70987fce57d361bb7a52
Mike Carr <mcarr@pachogrande.com>
2019-10-21 14:38:41 -0700

    fleshed out pytest tests to reflect different basesettings types and validations

--- test_sole.py
+++ test_sole.py
@@ -146,11 +146,45 @@ def test_SIMPLEPERSON_new():
 ###
 
 
+##### STRING TYPE
+
+
 def test_BASESETTINGS_getset_string():
     a = BaseSettings({"test_string": {"type": "string"}})
     v = "var"
     a.set("test_string", v)
     assert a.get("test_string") == v, "set/get don't match for string type"
+
+
+def test_BASESETTINGS_getset_string_notfloat():
+    a = BaseSettings({"test_string": {"type": "string"}})
+    v = 1.00
+    with pytest.raises(AssertionError, match="test_string.*string") as excinfo:
+        a.set("test_string", v)
+
+
+def test_BASESETTINGS_getset_string_notint():
+    a = BaseSettings({"test_string": {"type": "string"}})
+    v = 1
+    with pytest.raises(AssertionError, match="test_string.*string") as excinfo:
+        a.set("test_string", v)
+
+
+def test_BASESETTINGS_getset_string_notlist():
+    a = BaseSettings({"test_string": {"type": "string"}})
+    v = []
+    with pytest.raises(AssertionError, match="test_string.*string") as excinfo:
+        a.set("test_string", v)
+
+
+def test_BASESETTINGS_getset_string_notdict():
+    a = BaseSettings({"test_string": {"type": "string"}})
+    v = {}
+    with pytest.raises(AssertionError, match="test_string.*string") as excinfo:
+        a.set("test_string", v)
+
+
+##### FLOAT TYPE
 
 
 def test_BASESETTINGS_getset_float():
@@ -160,11 +194,73 @@ def test_BASESETTINGS_getset_float():
     assert a.get("test_float") == v, "set/get don't match for float type"
 
 
+def test_BASESETTINGS_getset_float_notstring():
+    a = BaseSettings({"test_float": {"type": "float"}})
+    v = "foo"
+    with pytest.raises(AssertionError, match="test_float.*float") as excinfo:
+        a.set("test_float", v)
+
+
+def test_BASESETTINGS_getset_float_intok():
+    a = BaseSettings({"test_float": {"type": "float"}})
+    v = 3
+    a.set("test_float", v)
+    assert a.get("test_float") == v, "set/get doesn't cast int type"
+
+
+def test_BASESETTINGS_getset_float_notlist():
+    a = BaseSettings({"test_float": {"type": "float"}})
+    v = []
+    with pytest.raises(AssertionError, match="test_float.*float") as excinfo:
+        a.set("test_float", v)
+
+
+def test_BASESETTINGS_getset_float_notdict():
+    a = BaseSettings({"test_float": {"type": "float"}})
+    v = {}
+    with pytest.raises(AssertionError, match="test_float.*float") as excinfo:
+        a.set("test_float", v)
+
+
+##### LIST TYPE
+
+
 def test_BASESETTINGS_getset_list():
     a = BaseSettings({"test_list": {"type": "list"}})
     v = [1, 2, 3]
     a.set("test_list", v)
     assert a.get("test_list") == v, "set/get don't match for list type"
+
+
+def test_BASESETTINGS_getset_list_notstring():
+    a = BaseSettings({"test_list": {"type": "list"}})
+    v = "foo"
+    with pytest.raises(AssertionError, match="test_list.*list") as excinfo:
+        a.set("test_list", v)
+
+
+def test_BASESETTINGS_getset_list_notfloat():
+    a = BaseSettings({"test_list": {"type": "list"}})
+    v = 1.00
+    with pytest.raises(AssertionError, match="test_list.*list") as excinfo:
+        a.set("test_list", v)
+
+
+def test_BASESETTINGS_getset_list_notint():
+    a = BaseSettings({"test_list": {"type": "list"}})
+    v = 1
+    with pytest.raises(AssertionError, match="test_list.*list") as excinfo:
+        a.set("test_list", v)
+
+
+def test_BASESETTINGS_getset_list_notdict():
+    a = BaseSettings({"test_list": {"type": "list"}})
+    v = {}
+    with pytest.raises(AssertionError, match="test_list.*list") as excinfo:
+        a.set("test_list", v)
+
+
+##### DICT TYPE
 
 
 def test_BASESETTINGS_getset_dict():
@@ -174,12 +270,46 @@ def test_BASESETTINGS_getset_dict():
     assert a.get("test_dict") == v, "set/get don't match for dict type"
 
 
+def test_BASESETTINGS_getset_dict_notstring():
+    a = BaseSettings({"test_dict": {"type": "dict"}})
+    v = "foo"
+    with pytest.raises(AssertionError, match="test_dict.*dict") as excinfo:
+        a.set("test_dict", v)
+
+
+def test_BASESETTINGS_getset_dict_notfloat():
+    a = BaseSettings({"test_dict": {"type": "dict"}})
+    v = 1.00
+    with pytest.raises(AssertionError, match="test_dict.*dict") as excinfo:
+        a.set("test_dict", v)
+
+
+def test_BASESETTINGS_getset_dict_notint():
+    a = BaseSettings({"test_dict": {"type": "dict"}})
+    v = 1
+    with pytest.raises(AssertionError, match="test_dict.*dict") as excinfo:
+        a.set("test_dict", v)
+
+
+def test_BASESETTINGS_getset_dict_notlist():
+    a = BaseSettings({"test_dict": {"type": "dict"}})
+    v = []
+    with pytest.raises(AssertionError, match="test_dict.*dict") as excinfo:
+        a.set("test_dict", v)
+
+
+##### COPY
+
+
 def test_BASESETTINGS_getset_iscopy():
     a = BaseSettings({"test_iscopy": {"type": "string"}})
     ov = "foo"
     a.set("test_iscopy", ov)
     nv = a.get("test_iscopy")
     assert nv is ov, "set/get are returning a copy rather than the original object"
+
+
+##### DEFAULTS
 
 
 def test_BASESETTINGS_getset_defaultsdosomething():
@@ -195,6 +325,49 @@ def test_BASESETTINGS_getset_defaultsdontoverride():
     a.set("test_default2", ov)
     nv = a.get("test_default2")
     assert nv == ov, "set is overriding default arguments when supplied"
+
+
+###### VALIDATION
+
+
+def test_BASESETTINGS_validation_gt_zero_ok():
+    a = BaseSettings({"test_float": {"type": "float", "validation": "gt_zero"}})
+    v = 1.00
+    a.set("test_float", v)
+
+
+def test_BASESETTINGS_validation_gt_zero_notok():
+    a = BaseSettings({"test_float": {"type": "float", "validation": "gt_zero"}})
+    v = -1.00
+    with pytest.raises(AssertionError, match="test_float") as excinfo:
+        a.set("test_float", v)
+
+
+def test_BASESETTINGS_validation_lt_zero_ok():
+    a = BaseSettings({"test_float": {"type": "float", "validation": "lt_zero"}})
+    v = -1.00
+    a.set("test_float", v)
+
+
+def test_BASESETTINGS_validation_lt_zero_notok():
+    a = BaseSettings({"test_float": {"type": "float", "validation": "lt_zero"}})
+    v = 1.00
+    with pytest.raises(AssertionError, match="test_float") as excinfo:
+        a.set("test_float", v)
+
+
+def test_BASESETTINGS_validation_is_percent_ok():
+    a = BaseSettings({"test_float": {"type": "float", "validation": "is_percent"}})
+    for i in range(100):
+        v = i / 100.00
+        a.set("test_float", v)
+
+
+def test_BASESETTINGS_validation_is_percent_notok():
+    a = BaseSettings({"test_float": {"type": "float", "validation": "is_percent"}})
+    v = 1.01
+    with pytest.raises(AssertionError, match="test_float") as excinfo:
+        a.set("test_float", v)
 
 
 ###
